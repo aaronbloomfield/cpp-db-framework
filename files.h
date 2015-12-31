e41b887cd80d8de1a1556cee86f88025e5345d43
Aaron Bloomfield <aaron@virginia.edu>
2015-12-30 23:34:57 -0500

    Can now handle SQLite3 databases as well as MySQL databases

--- files.h
+++ files.h
@@ -11,6 +11,11 @@ const char *dbobject_h = R"DBOBJH(//
 #include <vector>
 #include <string>
 #include <mysql/mysql.h>
+#include <sqlite3.h>
+
+#define DBOBJECT_CONNECTION_NONE 0
+#define DBOBJECT_CONNECTION_MYSQL 1
+#define DBOBJECT_CONNECTION_SQLITE 2
 
 using namespace std;
 
@@ -21,19 +26,20 @@ public:
   dbobject();
   virtual ~dbobject();
 
-  static bool connect(string host, string db, string user, string passwd);
-  static bool connect(char* host, char* db, char* user, char* passwd);
+  static bool connect_mysql(string host, string db, string user, string passwd);
+  static bool connect_mysql(char* host, char* db, char* user, char* passwd);
+  static bool connect_sqlite3(string filename);
   static void disconnect();
 
   static void setVerbose (bool which);
   static void setMySQLConnection(MYSQL *conn);
   static void saveAll(vector<dbobject*> vec, MYSQL *conn = NULL);
-  static void executeUpdate(string query, MYSQL *conn = NULL);
-  static unsigned int getLastInsertID(MYSQL *conn = NULL);
+  static void executeUpdate(string query);
+  static unsigned int getLastInsertID();
   static MYSQL *getMySQLConnection();
   static void raise_error(string s);
 
-  virtual void save(MYSQL *conn = NULL) = 0;
+  virtual void save() = 0;
   virtual string getTableName() = 0;
   virtual unsigned int size_in_bytes() = 0;
 
@@ -46,6 +52,8 @@ public:
 
 protected:
   static bool _verbose;
+  static int connection_status;
+  static sqlite3 *db;
 
   virtual dbobject* readInFullRow(MYSQL_ROW row) = 0;
   virtual ostream& put(ostream &out) = 0;
@@ -55,13 +63,19 @@ protected:
   static void possibly_call_reconnect_callback();
   static void increment_query_count();
 
+  static void executeUpdate_mysql(string query, MYSQL *conn = NULL);
+  static void executeUpdate_sqlite3(string query);
+  static unsigned int getLastInsertID_mysql(MYSQL *conn = NULL);
+  static unsigned int getLastInsertID_sqlite3();
+
 private:
   static vector<MYSQL*> conns;
 
   dbobject(const dbobject& orig);
 
-  static bool connect_private(const char* host, const char* db, const char* user, const char* passwd);
-  static bool connect_private();
+  static bool connect_mysql_private(const char* host, const char* db, const char* user, const char* passwd);
+  static bool connect_mysql_private();
+  static bool connect_sqlite3_private(string filename);
   static int num_reconnects;
   static string dbhost, dbpass, dbuser, dbname;
   static void (*func)(int);
@@ -69,6 +83,8 @@ private:
   static void verify_conn_is_open();
   static void set_db_credentials (string host, string name, string user, string pass);
   static void set_db_credentials (char *host, char *name, char *user, char *pass);
+ protected:
+  static int last_insert_rowid_callback(void *last_id, int argc, char **argv, char **azColName);
 
   friend ostream& operator<< (ostream& out, dbobject &x);
 
@@ -94,6 +110,7 @@ const char *dbobject_c = R"DBOBJC(//
 #include <stdio.h>
 #include <assert.h>
 #include <omp.h>
+#include <cstdlib>
 
 #include "dbobject.h"
 
@@ -110,7 +127,8 @@ int dbobject::num_reconnects = 0;
 bool dbobject::_verbose = false;
 unsigned long dbobject::query_count = 0;
 vector<MYSQL*> dbobject::conns = vector<MYSQL*>();
-
+int dbobject::connection_status = DBOBJECT_CONNECTION_NONE;
+sqlite3* dbobject::db = NULL;
 
 dbobject::dbobject() {
 }
@@ -149,7 +167,8 @@ bool dbobject::reconnect() {
   }
 #pragma omp critical(output)
   cerr << "Attempting to reconnect to the DB in thread " << omp_get_thread_num() << " (" << num_reconnects-- << " more attempts allowed)..." << endl;
-  if ( connect_private() ) {
+  setMySQLConnection(NULL);
+  if ( connect_mysql_private() ) {
 #pragma omp critical(output)
     cerr << "Reconnection attempt successful in thread " << omp_get_thread_num() << endl;
     return true;
@@ -174,13 +193,21 @@ void dbobject::possibly_call_reconnect_callback() {
 }
 
 void dbobject::verify_conn_is_open() {
-  unsigned int thread_num = omp_get_thread_num();
+  if ( connection_status == DBOBJECT_CONNECTION_MYSQL ) {
+    unsigned int thread_num = omp_get_thread_num();
+    if ( thread_num >= conns.size() ) {
 #pragma omp critical(dbcppconns)
-  while ( thread_num >= conns.size() )
-    conns.push_back(NULL);
-  if ( conns[thread_num] == NULL )
-    connect_private();
-  assert(conns[thread_num]!=NULL);
+      while ( thread_num >= conns.size() )
+	conns.push_back(NULL);
+    }
+    if ( conns[thread_num] == NULL )
+      connect_mysql_private();
+    assert(conns[thread_num]!=NULL);
+  } else if ( connection_status == DBOBJECT_CONNECTION_SQLITE ) {
+    assert(db != NULL);
+  } else {
+    assert(0);
+  }
 }
 
 unsigned long dbobject::get_query_count() {
@@ -192,50 +219,87 @@ void dbobject::increment_query_count() {
   query_count++;
 }
 
-bool dbobject::connect_private() {
+bool dbobject::connect_mysql_private() {
   //assert(dbpass!=""); // pasword can be empty
   assert(dbhost!="");
   assert(dbuser!="");
   assert(dbname!="");
-  return connect_private(dbhost.c_str(),dbname.c_str(),dbuser.c_str(),dbpass.c_str());
+  return connect_mysql_private(dbhost.c_str(),dbname.c_str(),dbuser.c_str(),dbpass.c_str());
 }
 
-bool dbobject::connect_private(const char* host, const char* db, const char* user, const char* passwd) {
-  set_db_credentials(host,db,user,passwd);
+bool dbobject::connect_mysql_private(const char* host, const char* db, const char* user, const char* passwd) {
+  assert((connection_status == DBOBJECT_CONNECTION_NONE) || (connection_status == DBOBJECT_CONNECTION_MYSQL));
+  unsigned int thread_num = omp_get_thread_num();
+  if ( (connection_status == DBOBJECT_CONNECTION_MYSQL) &&
+       (conns.size() > thread_num) &&
+       (conns[thread_num] != NULL) )
+    return true;
   MYSQL *conn = mysql_init(NULL);
   if (!mysql_real_connect(conn, dbhost.c_str(), dbuser.c_str(), dbpass.c_str(), dbname.c_str(), 0, NULL, 0)) {
     cout << "\tconnection error: " << mysql_error(conn) << endl;
     return false;
   }
-  setMySQLConnection(conn);
+  setMySQLConnection(conn); // sets connection_status also
   return true;
 }
 
-bool dbobject::connect(char* host, char* db, char* user, char* passwd) {
+bool dbobject::connect_mysql(char* host, char* db, char* user, char* passwd) {
   set_db_credentials(host,db,user,passwd);
   bool ret;
 #pragma omp critical(dbcpp)
-  ret = connect_private(host, db, user, passwd);
+  ret = connect_mysql_private(host, db, user, passwd);
   return ret;
 }
 
-bool dbobject::connect(string host, string db, string user, string passwd) {
+bool dbobject::connect_mysql(string host, string db, string user, string passwd) {
   set_db_credentials(host,db,user,passwd);
   bool ret;
 #pragma omp critical(dbcpp)
-  ret = connect_private(host.c_str(),db.c_str(),user.c_str(),passwd.c_str());
+  ret = connect_mysql_private(host.c_str(),db.c_str(),user.c_str(),passwd.c_str());
   return ret;
+}
+
+bool dbobject::connect_sqlite3(string filename) {
+  bool ret;
+#pragma omp critical(dbcpp)
+  ret = connect_sqlite3_private(filename.c_str());
+  return ret;
+}
+
+bool dbobject::connect_sqlite3_private(string filename) {
+  if ( connection_status == DBOBJECT_CONNECTION_SQLITE )
+    return true;
+  assert( connection_status == DBOBJECT_CONNECTION_NONE );
+  assert(db == NULL);
+  int rc = sqlite3_open(filename.c_str(), &db);
+  if ( rc ) {
+    cerr << "Can't open sqlite3 database '" << filename << "': " << sqlite3_errmsg(db) << endl;
+    return false;
+  }
+  connection_status = DBOBJECT_CONNECTION_SQLITE;
+  return true;
 }
 
 void dbobject::disconnect() {
 #pragma omp critical(dbcppconns)
   {
-    for ( unsigned int i = 0; i < conns.size(); i++ ) {
-      if ( conns[i] != NULL )
-        mysql_close(conns[i]);
-      conns[i] = NULL;
+    if ( connection_status == DBOBJECT_CONNECTION_SQLITE ) {
+      assert(db != NULL);
+      sqlite3_close(db);
+      db = NULL;
+    } else if ( connection_status == DBOBJECT_CONNECTION_MYSQL ) {
+      for ( unsigned int i = 0; i < conns.size(); i++ ) {
+	if ( conns[i] != NULL )
+	  mysql_close(conns[i]);
+	conns[i] = NULL;
+      }
+    } else if ( connection_status == DBOBJECT_CONNECTION_NONE ) {
+      // do nothing
+    } else {
+      assert(0);
     }
     conns.clear();
+    connection_status = DBOBJECT_CONNECTION_NONE;
   }
 }
 
@@ -244,25 +308,65 @@ void dbobject::setVerbose(bool which) {
 }
 
 void dbobject::setMySQLConnection(MYSQL *conn) {
+  assert((connection_status == DBOBJECT_CONNECTION_NONE) || (connection_status == DBOBJECT_CONNECTION_MYSQL));
   unsigned int thread_num = omp_get_thread_num();
+  if ( thread_num >= conns.size() ) {
 #pragma omp critical(dbcppconns)
-  while ( thread_num >= conns.size() )
-    conns.push_back(NULL);
+    while ( thread_num >= conns.size() )
+      conns.push_back(NULL);
+  }
   conns[thread_num] = conn;
+  connection_status = DBOBJECT_CONNECTION_MYSQL;
 }
 
 MYSQL* dbobject::getMySQLConnection() {
-  if ( conns.size() == 0 )
-    return NULL;
-  verify_conn_is_open();
+  assert(connection_status == DBOBJECT_CONNECTION_MYSQL);
   unsigned int thread_num = omp_get_thread_num();
-  if ( thread_num < conns.size() )
-    return conns[thread_num];
-  else
-    return NULL;
+  if ( thread_num >= conns.size() ) {
+#pragma omp critical(dbcppconns)
+    while ( thread_num >= conns.size() )
+      conns.push_back(NULL);
+  }
+  verify_conn_is_open();
+  return conns[thread_num];
 }
 
-unsigned int dbobject::getLastInsertID(MYSQL *conn) {
+unsigned int dbobject::getLastInsertID() {
+  if ( connection_status == DBOBJECT_CONNECTION_MYSQL )
+    return getLastInsertID_mysql();
+  else if ( connection_status == DBOBJECT_CONNECTION_SQLITE )
+    return getLastInsertID_sqlite3();
+  else {
+    assert(0);
+    return 0;
+  }
+}
+
+int dbobject::last_insert_rowid_callback(void *last_id, int argc, char **argv, char **azColName){
+  assert(argc == 1);
+  assert(last_id != NULL);
+  int *id = (int*) last_id;
+  *id = atoi(argv[0]);
+  return 0;
+}
+
+unsigned int dbobject::getLastInsertID_sqlite3() {
+  assert(connection_status == DBOBJECT_CONNECTION_SQLITE);
+  char* errorMsg;
+  unsigned int last_id = 0;
+  int rc = 0;
+#pragma omp critical(dbcpp)
+  rc = sqlite3_exec(db, "select last_insert_rowid()", last_insert_rowid_callback, &last_id, &errorMsg);
+  if ( rc != SQLITE_OK ) {
+    cout << "SQLite3 error: " << errorMsg << endl;
+    sqlite3_free(errorMsg);
+  }
+  return last_id;
+}
+
+unsigned int dbobject::getLastInsertID_mysql(MYSQL *conn) {
+  unsigned int thread_num = omp_get_thread_num();
+  assert(connection_status == DBOBJECT_CONNECTION_MYSQL);
   verify_conn_is_open();
   if ( conn == NULL )
     conn = getMySQLConnection();
@@ -289,7 +393,7 @@ unsigned int dbobject::getLastInsertID(MYSQL *conn) {
     recret = reconnect();
     if ( recret ) {
       possibly_call_reconnect_callback();
-      cerr << "Calling method again in thread " << omp_get_thread_num() << "..." << endl;
+      cerr << "Calling method again in thread " << thread_num << "..." << endl;
       return getLastInsertID(); // does NOT send in the 'conn' parameter
     } else {
       cerr << "Unable to reconnect to database." << endl;
@@ -299,7 +403,30 @@ unsigned int dbobject::getLastInsertID(MYSQL *conn) {
   return id;
 }
 
-void dbobject::executeUpdate(string query, MYSQL *conn) {
+void dbobject::executeUpdate(string query) {
+  if ( connection_status == DBOBJECT_CONNECTION_MYSQL )
+    executeUpdate_mysql(query);
+  else if ( connection_status == DBOBJECT_CONNECTION_SQLITE )
+    executeUpdate_sqlite3(query);
+  else
+    assert(0);
+}
+
+void dbobject::executeUpdate_sqlite3(string query) {
+  assert(connection_status == DBOBJECT_CONNECTION_SQLITE);
+  char* errorMsg;
+  int rc = 0;
+#pragma omp critical(dbcpp)
+  rc = sqlite3_exec(db, query.c_str(), NULL, NULL, &errorMsg);
+  if ( rc != SQLITE_OK ) {
+    cout << "SQLite3 error: " << errorMsg << endl;
+    sqlite3_free(errorMsg);
+  }
+}
+
+void dbobject::executeUpdate_mysql(string query, MYSQL *conn) {
+  unsigned int thread_num = omp_get_thread_num();
+  assert(connection_status == DBOBJECT_CONNECTION_MYSQL);
   verify_conn_is_open();
   if ( conn == NULL )
     conn = getMySQLConnection();
@@ -315,13 +442,13 @@ void dbobject::executeUpdate(string query, MYSQL *conn) {
   if ( !isgood ) {
     string error = mysql_error(conn);
 #pragma omp critical(output)
-    cerr << error << " in thread " << omp_get_thread_num() << " in dbobject::executeUpdate() on query: " << query << endl;
+    cerr << error << " in thread " << thread_num << " in dbobject::executeUpdate() on query: " << query << endl;
     bool recret = false;
 #pragma omp critical(dbcpp)
     recret = reconnect();
     if ( recret ) {
       possibly_call_reconnect_callback();
-      cerr << "Calling method again in thread " << omp_get_thread_num() << "..." << endl;
+      cerr << "Calling method again in thread " << thread_num << "..." << endl;
       return executeUpdate(query); // does NOT send in the 'conn' parameter
     } else {
       cerr << "Unable to reconnect to database." << endl;
@@ -354,5 +481,3 @@ ostream& operator<< (ostream& out, dbobject &x) {
 
 } // namespace db
 )DBOBJC";
-
-//--------------------------------------------------------------------------------
